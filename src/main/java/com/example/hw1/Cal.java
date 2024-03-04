fb95c0e8cde9d90acc89d0b3785df6026986da16
Mohammad <zare.6984@gmail.com>
2024-03-04 19:10:16 +0330

    completing calculating simple mathematics operations

--- src/main/java/com/example/hw1/Cal.java
+++ src/main/java/com/example/hw1/Cal.java
@@ -1,12 +1,24 @@
 package com.example.hw1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Cal {
-    @GetMapping("cal/[num1]/[num2]")
-    public void sum(){
+    @GetMapping("{operation}/{num1}/{num2}")
+    public float sum(@PathVariable int num1 , @PathVariable int num2 , @PathVariable String operation){
+        return switch (operation) {
+            case "sum" -> num1 + num2;
+
+            case "mul" -> num1 * num2;
+
+            case "sub" -> num1 - num2;
+
+            case "div" -> (float) num1 / num2;
+
+            default -> -1;
+        };
 
     }
 }

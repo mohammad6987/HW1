ddacbe7ffd1c626f0e8dc571bc57194e29f4fc9d
Mohammad <zare.6984@gmail.com>
2024-03-04 15:17:47 +0330

    test commit

--- src/main/java/com/example/hw1/Cal.java
+++ src/main/java/com/example/hw1/Cal.java
@@ -1,2 +1,12 @@
-package com.example.hw1;public class Cal {
+package com.example.hw1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Cal {
+    @GetMapping("cal/[num1]/[num2]")
+    public void sum(){
+
+    }
 }

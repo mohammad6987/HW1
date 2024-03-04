1d7cc4b9bc2becc97dbe7f9cac3829742810baa3
Mohammad <zare.6984@gmail.com>
2024-03-05 00:42:29 +0330

    nothing

--- src/main/java/com/example/hw1/Calculator.java
+++ src/main/java/com/example/hw1/Calculator.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Cal {
+public class Calculator {
     @GetMapping("{operation}/{num1}/{num2}")
     public float sum(@PathVariable int num1 , @PathVariable int num2 , @PathVariable String operation){
 

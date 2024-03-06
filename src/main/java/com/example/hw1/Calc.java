b30b28862c64c472554213d2f832001718bfff5b
Mohammad <zare.6984@gmail.com>
2024-03-06 12:36:19 +0330

    changing names of pathVariables

--- src/main/java/com/example/hw1/Calc.java
+++ src/main/java/com/example/hw1/Calc.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Calculator {
+public class Calc {
     @GetMapping("{operation}/{num1}/{num2}")
     public float sum(@PathVariable float num1 , @PathVariable float num2 , @PathVariable String operation){
 
         return switch (operation) {
             case "sum" -> num1 + num2;
 
-            case "mul" -> num1 * num2;
+            case "mult" -> num1 * num2;
 
-            case "sub" -> num1 - num2;
+            case "subtract" -> num1 - num2;
 
             case "div" ->  num1 / num2;
 
